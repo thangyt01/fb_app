f2e9b2833192f4effa50c9eea54438c25cbb0c73
cnh1en <cnhien12@gmail.com>
2022-10-25 00:15:28 +0700

    refact: login screen

--- components/screen/Login.jsx
+++ components/screen/Login.jsx
@@ -1,41 +1,83 @@
-import React from 'react';
-import { Image, Text, TextInput, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+/* eslint-disable react-native/no-color-literals */
+/* eslint-disable react-native/no-unused-styles */
+import axios from 'axios';
+import React, { useState } from 'react';
+import {
+	Button,
+	Image,
+	ScrollView,
+	StatusBar,
+	Text,
+	TextInput,
+	View,
+} from 'react-native';
 import { useTailwind } from 'tailwind-rn/dist';
-import facebookLogo from '../../assets/facebook-logo.png';
-import StyledButton from '../common/Button';
+import facebookLogo from '../../assets/banner.png';
 
 const Login = () => {
 	const tw = useTailwind();
 
-	return (
-		<SafeAreaView style={tw('bg-white h-full px-8')}>
-			<View>
-				<View style={tw('mb-28 mt-8')}>
-					<Image
-						source={facebookLogo}
-						style={tw('w-32 h-32 mb-36 self-center')}
-					/>
-					<TextInput
-						placeholder="Phone or email"
-						style={tw('py-1 border-b border-[#AAAAAA] mb-4')}
-					/>
-					<TextInput
-						placeholder="Password"
-						style={tw('py-1 border-b border-[#AAAAAA] mb-6')}
-					/>
-					<StyledButton title="Login" bg="bg-[#007AFF]" />
-				</View>
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-				<View>
-					<Text style={tw('self-center underline')}>
-						Forgotten the password ?
-					</Text>
-					<Text style={tw('self-center font-bold uppercase my-2')}>Or</Text>
-					<StyledButton title="Create a new account" bg="bg-[#A4C939]" />
+	const handleLogin = async () => {
+		try {
+			const response = await axios.post('http://bkwatch.me:8010/api/login', {
+				username,
+				password,
+			});
+			console.log(response.data);
+		} catch (error) {
+			console.log(error.response);
+		}
+	};
+
+	return (
+		<ScrollView style={tw('bg-white h-full')}>
+			<StatusBar
+				barStyle="dark-content"
+				backgroundColor="#30477C"
+				translucent={true}
+			/>
+
+			<View style={tw('flex-12')}>
+				<View style={tw('flex-[1.7]')}>
+					<Image source={facebookLogo} style={tw('w-full h-60')} />
+				</View>
+				<View style={tw('flex-3 mx-5')}>
+					<View style={tw('mt-5')}>
+						<TextInput
+							style={tw('h-10 border-b-[1px] mb-3 border-b-[#ECEAEC]')}
+							placeholder="Email or username"
+							onChangeText={(inputEmail) => setUsername(inputEmail)}
+						/>
+						<TextInput
+							style={tw('h-10 border-b-[1px] mb-3 border-b-[#ECEAEC]')}
+							placeholder="Password"
+							secureTextEntry={true}
+							onChangeText={(inputPassword) => setPassword(inputPassword)}
+						/>
+					</View>
+					<View style={tw('mt-3')}>
+						<Button
+							title="Login"
+							color="#4E68A2"
+							onPress={handleLogin}
+							disabled={!username || !password}
+						/>
+					</View>
+					<Text style={tw('mt-5 self-center')}>Forgotten password?</Text>
+				</View>
+				<View style={tw('flex-row mt-5')}>
+					<View style={tw('flex-grow border-b border-b-[#ddd]')} />
+					<Text style={tw('-mb-2 mx-1')}>OR</Text>
+					<View style={tw('flex-grow border-b border-b-[#ddd]')} />
+				</View>
+				<View style={tw('mt-5 mx-6 justify-center items-center')}>
+					<Button title="Create account" color="#07A007" />
 				</View>
 			</View>
-		</SafeAreaView>
+		</ScrollView>
 	);
 };
 
